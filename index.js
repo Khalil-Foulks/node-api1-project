fab4ab8538453e9c0b418fd2aeeb0b8f5faff704
Khalil <khalilfoulks@gmail.com>
2020-08-03 17:13:26 -0400

    added requirements to get and post requests

--- index.js
+++ index.js
@@ -11,20 +11,41 @@ server.post('/api/users', (req, res) => {
     const user = req.body
     user.id = shortid.generate();
 
-    users.push(user)
-    res.status(201).json(users)
+    try{
+        if(!user.name || !user.bio){
+            res.status(400).json({errorMessage: "Please provide a name and bio for the user."})
+        } else {
+            users.push(user)
+            res.status(201).json(users)
+        }
+    } catch(err){
+        res.status(500).json({errorMessage: "There was an error while saving the user to the database"});
+    }
 })
 
 server.get('/api/users', (req, res) => {
-    res.status(200).json(users)
+    
+    try{
+        res.status(200).json(users)
+    } catch(err){
+        res.status(500).json({errorMessage: "The users information could not be retrieved."});
+    }
 })
 
 server.get('/api/users/:id', (req, res) => {
     const id = req.params.id;
+    found = users.find(u => u.id === id);
+    
+    try{
+        if(found){
+            res.status(200).json(found)
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
+    } catch(err){
+        res.status(500).json({errorMessage: "The user information could not be retrieved."});
+    }
 
-    users = users.filter(u => u.id === id);
-
-    res.status(200).json(users)
 })
 
 server.delete('/api/users/:id', (req, res) => {
@@ -45,6 +66,18 @@ server.put('/api/users/:id', (req, res) => {
         res.status(200).json(users);
     } else {
         res.status(404).json({ message: `There is no account with id ${id}` })
+    }
+
+    
+    try{
+        if(!user.name || !user.bio){
+            res.status(400).json({errorMessage: "Please provide a name and bio for the user."})
+        } else {
+            users.push(user)
+            res.status(201).json(users)
+        }
+    } catch(err){
+        res.status(500).json({errorMessage: "There was an error while saving the user to the database"});
     }
 });
 
